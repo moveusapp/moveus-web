ca41c41f7412e90352cf097007022671bcbf5b57
Filip <filipsmolcec@gmail.com>
2026-05-17 20:10:06 +0200

    Remove unused components

--- src/utils/image-data.ts
+++ src/utils/image-data.ts
@@ -1,47 +1,3 @@
-function getCroppedImg(
-  imageSrc: string,
-  pixelCrop: { x: number; y: number; width: number; height: number },
-): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const image = new Image();
-    image.src = imageSrc;
-    image.crossOrigin = "anonymous"; // important if using images from external URLs
-
-    image.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = pixelCrop.width;
-      canvas.height = pixelCrop.height;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return reject(new Error("Could not get canvas context"));
-
-      ctx.drawImage(
-        image,
-        pixelCrop.x,
-        pixelCrop.y,
-        pixelCrop.width,
-        pixelCrop.height,
-        0,
-        0,
-        pixelCrop.width,
-        pixelCrop.height,
-      );
-
-      const base64Image = canvas.toDataURL("image/jpeg");
-
-      resolve(base64Image);
-    };
-
-    image.onerror = (err) => reject(err);
-  });
-}
-
-function base64ToBlob(base64: string, mime = "image/jpeg"): Blob {
-  const byteChars = atob(base64.split(",")[1]);
-  const byteNumbers = Array.from(byteChars).map((c) => c.charCodeAt(0));
-  const byteArray = new Uint8Array(byteNumbers);
-  return new Blob([byteArray], { type: mime });
-}
-
 async function fileToBase64(file: File): Promise<string> {
   try {
     const reader = new FileReader();
@@ -56,4 +12,4 @@ async function fileToBase64(file: File): Promise<string> {
   }
 }
 
-export { getCroppedImg, base64ToBlob, fileToBase64 };
+export { fileToBase64 };
