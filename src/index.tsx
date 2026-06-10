34769e5e0c1add1862e36c8897c2033cac6a4a96
Filip <filipsmolcec@gmail.com>
2026-06-11 01:55:38 +0200

    Fix? toast import bug

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/styles/index.css";
 import "@/styles/reuseable.css";
-import App from "./App";
+import App from "@/App";
 import { ApolloProvider } from "@apollo/client/react";
-import { apolloClient } from "./apollo/client";
-import { UserProvider } from "./context/profile-context";
-import { ToastProvider } from "./context/toast-context";
-import { LanguageProvider } from "./context/language-context";
-import { getStoredLocale, loadLocale } from "./translations/strings";
+import { apolloClient } from "@/apollo/client";
+import { UserProvider } from "@/context/profile-context";
+import { ToastProvider } from "@/context/toast-context";
+import { LanguageProvider } from "@/context/language-context";
+import { getStoredLocale, loadLocale } from "@/translations/strings";
 
 void loadLocale(getStoredLocale()).then(() => {
   const root = ReactDOM.createRoot(
